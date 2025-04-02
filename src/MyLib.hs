060251e9ac0e9f95592c307b559b50eaad502325
nrs <seba@localhost>
2025-04-02 08:47:34 +0000

    newcommit

--- src/MyLib.hs
+++ src/MyLib.hs
@@ -1,11 +1,37 @@
 module MyLib where
 
 import Data.List
+import Lens.Micro.Pro hiding (traversed)
+import Numeric.Natural
+import Data.Map (Map, fromList)
+import Data.Char
+import Control.Lens.Combinators (dropping, taking, takingWhile)
+import Control.Lens.Traversal
 
-tuplize :: Int -> [a] -> [[a]]
-tuplize n = takeWhile ((== n) . length) . map (take n) . tails
+instance Semigroup Int where
+  (<>) = (+)
+
+instance Monoid Int where
+  mempty = 0
+  mappend = (<>)
+
+tuplize :: Integral integral => integral -> [a] -> [[a]]
+tuplize n = takeWhile ((== n) . fromIntegral . length) . map (take (fromIntegral n)) . tails
+
+tuplizeIso :: Integral integral => integral -> Iso' [a] [[a]]
+tuplizeIso n = iso (tuplize n) (map head)
 
 {-# HLINT ignore tuplize_overlapless #-}
-tuplize_overlapless :: Int -> [a] -> [[a]]
-tuplize_overlapless n = takeWhile ((== n) . length) . map (take n) . iterate (drop n)
+tuplize_overlapless :: Integral integral => integral -> [a] -> [[a]]
+tuplize_overlapless n = takeWhile ((== n) . fromIntegral . length) . map (take $ fromIntegral n) . iterate (drop $ fromIntegral n)
 
+enumerateIso :: Iso' [a] [(Natural, a)]
+enumerateIso = iso (zip [0..]) (map snd)
+
+
+-- modUsingEnumeration :: Int -> Lens [a] [(Int, a)] a (Int, a)
+-- modUsingEnumeration n = lens 
+--   (!! fromIntegral n)
+--   (\s b -> 
+--     let x = _ in
+--     _)
